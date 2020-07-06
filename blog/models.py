9ebcfbb4ef57e12452e989552da04666e99b3868
ninanolets <marinaa.noleto@gmail.com>
2020-07-06 15:52:36 +0200

    Added bootstrap and jobs are showing up

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+
+
+
+
+# Create a blog models
+    # title
+    # pub_date
+    # body
+    # image
+# Add the blog app to the settings
+# Create a migration
+# Migrate
+# Add to the admin
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Job
 # Create your views here.
+
+def home(request):
+    jobs = Job.objects # this will get all the job models into the database and turn them into python objects
+    return render(request, 'jobs/home.html', {'jobs': jobs})
+
+# Make homepage: this, together with url path
